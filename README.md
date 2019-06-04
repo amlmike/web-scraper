0a45e1b8398fcd0c52f434063179bfa6ae46d988
“amlmike” <amandalmikesell@gmail.com>
2019-06-03 21:08:27 -0400

    restructure with goose3

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A first project for a new programmer.  Attempting to modify code aquired in the 
 https://www.youtube.com/watch?v=7SWVXPYZLJM
 https://2.python-requests.org/en/master/
 BeautifulSoup
+https://github.com/grangier/python-goose
+https://goose3.readthedocs.io/en/latest/quickstart.html#setup
--- web-scraper.py
+++ web-scraper.py
@@ -3,7 +3,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.cnn.com/2019/06/03/politics/donald-trump-queen-elizabeth-ii-buckingham-palace-banquet/index.html"
+url = "https://www.cnn.com/2019/06/03/politics/jared-kushner-axios/index.html"
 resp = requests.get(url)
 
 soup = BeautifulSoup(resp.text, 'html.parser')
+print(soup.prettify())
+
+from goose3 import Goose
+g = Goose()
+article = g.extract(url='https://www.cnn.com/2019/06/03/politics/jared-kushner-axios/index.html')
+print(article.cleaned_text)
+g.close()
